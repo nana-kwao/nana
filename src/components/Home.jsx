94ae4f82fad978c7c737abbc9bb6bfeca88934e3
nana-kwao <nanakwaofreiku@outlook.com>
2025-08-25 22:05:29 +0000

    minor update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,24 +2,36 @@ import {
   CourseTitle,
   SubjectHeading,
   AuthWelcomeText,
+  AuthBackground,
+  AuthBackGroundWrapper,
   AuthBackgroundWrapperDiv,
 } from "milesuicomponents";
 
 const Home = () => {
   return (
     <>
-      <AuthBackgroundWrapperDiv id="home" className="home-area">
-        <CourseTitle style={{ fontSize: "28px" }}>
-          Welcome to my Resume Website
-        </CourseTitle>
-        <SubjectHeading style={{ fontSize: "20px" }}>
-          I'm Freiku Nana Kwao
-        </SubjectHeading>
-        <AuthWelcomeText>
-          A Passionate full-stack javascript web developer with a drive to build
-          impactful solutions
-        </AuthWelcomeText>
-      </AuthBackgroundWrapperDiv>
+      <AuthBackground>
+        <AuthBackGroundWrapper>
+          <AuthBackgroundWrapperDiv
+            id="home"
+            style={{
+              color: "#fff",
+              textAlign: "center",
+            }}
+          >
+            <CourseTitle style={{ fontSize: "38px" }}>
+              Welcome to my Resume Website
+            </CourseTitle>
+            <SubjectHeading style={{ fontSize: "30px" }}>
+              I'm Freiku Nana Kwao
+            </SubjectHeading>
+            <AuthWelcomeText style={{padding: "1em"}}>
+              A Passionate full-stack javascript web developer with a drive to
+              build impactful solutions
+            </AuthWelcomeText>
+          </AuthBackgroundWrapperDiv>
+        </AuthBackGroundWrapper>
+      </AuthBackground>
     </>
   );
 };
